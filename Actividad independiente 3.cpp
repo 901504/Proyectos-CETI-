4852452f41a08b60c61bb0f7abd2968488b00698
jair <jair901504andres@gmail.com>
2022-11-28 22:24:03 -0600

    se agrega el proyecto

---
+++ Actividad independiente 3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+    int m,ca, ah;
+   
+    printf ("indica cuanto ahorro en el mes\n\n");
+	 scanf ("%d", &m);
+    
+    ca=m *30 * 12;
+    printf ("la cantidad de ahorro al ano es %d\n\n", ca);
+    
+    return 0;
+    
+	
+	
+	
+}
